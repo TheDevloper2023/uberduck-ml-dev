a3bd3ab9745e01e5a7fce0b0409310657f68271a
TheDevloper2023 <154549023+TheDevloper2023@users.noreply.github.com>
2025-07-20 13:00:21 +0300

    Update torchmoji.py
    Works with Python 3.11 (Colab)

--- uberduck_ml_dev/models/torchmoji.py
+++ uberduck_ml_dev/models/torchmoji.py
@@ -542,7 +542,7 @@ TITLES = [
     r"Prof\.",
 ]
 # Ensure case insensitivity
-RE_TITLES = r"|".join([r"(?i)" + t for t in TITLES])
+RE_TITLES = r"|".join(TITLES)
 
 # Symbols have to be created as separate patterns in order to match consecutive
 # identical symbols.
@@ -643,7 +643,7 @@ IGNORED = [RE_WHITESPACE]
 
 # Final pattern
 RE_PATTERN = re.compile(
-    r"|".join(IGNORED) + r"|(" + r"|".join(TOKENS) + r")", re.UNICODE
+    r"|".join(IGNORED) + r"|(" + r"|".join(TOKENS) + r")", re.UNICODE | re.IGNORECASE
 )
 
 
